e29cbe42fa93fa8c9295781aeb10b2fc7f7c55a0
Thedogecraft <dantaealt@gmail.com>
2025-09-18 18:09:27 -0700

    fix: remove problematic tweak
    causes major issues on the latest version of windows

--- resources/tweaks/disable-notification-center/apply.ps1
+++
@@ -1,9 +0,0 @@
-# Ensure the registry path exists
-New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows" -Name "Explorer" -Force | Out-Null
-
-# Disable Notification Center (Action Center)
-Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
-
-# Restart Explorer to apply changes
-Stop-Process -Name explorer -Force
-Start-Process explorer
--- resources/tweaks/disable-notification-center/unapply.ps1
+++
@@ -1,6 +0,0 @@
-# Re-enable Notification Center by setting the value to 0
-Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 0
-
-# Restart Explorer to apply changes
-Stop-Process -Name explorer -Force
-Start-Process explorer
